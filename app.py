50d51e59f562a9ee22a773f35cbffeb8b103f505
moli989 <sweetmoli89@gmail.com>
2025-07-22 19:45:46 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -54,4 +54,5 @@ def chat():
 
 if __name__ == "__main__":
     import os
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 10000))  # Render 会设置 PORT 环境变量
+    app.run(host="0.0.0.0", port=port)

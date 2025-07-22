07ad4779b741a2783690e6e60dc1fd9d2d525033
moli989 <sweetmoli89@gmail.com>
2025-07-22 11:41:56 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def chat():
 
             transcript = client.audio.transcriptions.create(
                 model="whisper-1",
-                file=open(tmp_audio.name, "rb")，
+                file=open(tmp_audio.name, "rb"),
                 language="en"
             )
             question = transcript.text
